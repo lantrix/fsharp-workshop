0b2865b006dace0f6a31cdd9cbab95ac458e1f24
Andrew Browne <brownie@brownie.com.au>
2015-07-26 23:07:02 +1000

    Temp

---
+++ chatter/Chatter.Server/ChatPage.fs
@@ -0,0 +1,34 @@
+﻿namespace Chatter.Server
+
+open FSharpx.Collections
+
+module ChatPage =
+    let renderShell inner = seq {
+        yield 
+            """<html>
+                 <head><title>Chatter</title></head>
+                 <body>"""
+
+        yield! inner
+
+        yield  
+            """  </body>
+               </html>""" }
+
+    let renderRoom (room : Room) = seq {
+        yield sprintf """<a href="/room/%s">%s</a>""" room room
+    }
+
+    let renderRoomList (rooms : list<Room>) = seq {
+        yield "<ul>"
+
+        for room in rooms do
+            yield "<li>"
+            yield! renderRoom room
+            yield "</li>"
+
+        yield "</ul>"
+    }
+        
+    let render (pageState : PageState) = 
+       renderShell (renderRoomList pageState.Rooms)
--- chatter/Chatter.Server/Program.fs
+++ chatter/Chatter.Server/Program.fs
@@ -1,4 +1,5 @@
-﻿open Suave
+﻿namespace Chatter.Server
+open Suave
 open Suave.Http.Successful
 open Suave.Http
 open Suave.Http.Applicatives
@@ -7,18 +8,24 @@ open Suave.Http.RequestErrors
 open Suave.Web
 open Suave.Types
 
-[<EntryPoint>]
-let main argv = 
-    printfn "Current directory: %s"  <| System.IO.Directory.GetCurrentDirectory()
+module Main = 
+    [<EntryPoint>]
+    let main argv = 
+        printfn "Current directory: %s"  <| System.IO.Directory.GetCurrentDirectory()
 
-    choose [
-        GET >>= path "/" >>= file "./static/index.html"
-        GET >>= pathScan "/static/%s" (fun s -> (file <| sprintf "./static/%s" s))
-        NOT_FOUND "Not found"
-    ]
-    |> startWebServer {
-        defaultConfig with 
-            logger = Suave.Logging.Loggers.saneDefaultsFor Logging.LogLevel.Debug
-    }
+        choose [
+            GET 
+                >>= path "/" 
+                >>=
+                    ({ PageState.Rooms = ["Room1";"Room2"]; CurrentRoom = None }
+                     |> ChatPage.render
+                     |> toHtml)
+            GET >>= pathScan "/static/%s" (fun s -> (file <| sprintf "./static/%s" s))
+            NOT_FOUND "Not found"
+        ]
+        |> startWebServer {
+            defaultConfig with 
+                logger = Suave.Logging.Loggers.saneDefaultsFor Logging.LogLevel.Debug
+        }
 
-    0 // return an integer exit code
+        0 // return an integer exit code
